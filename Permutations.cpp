2c876e398e2f53c2c8088d05a8ae258c773c1d42
zee-1 <zaid.khan21800@gmail.com>
2023-07-10 19:25:12 +0530

    On branch master Changes to be committed: 	deleted:    2_sum 	deleted:    Increasing_Array 	deleted:    Permutations 	modified:   Permutations.cpp 	deleted:    collecting_num 	deleted:    collecting_numII 	deleted:    colorGame 	deleted:    increasing_arr 	deleted:    maxSubarray 	deleted:    missing_num 	deleted:    playlist 	deleted:    repetitions 	deleted:    weird_algo

--- Permutations.cpp
+++ Permutations.cpp
@@ -1,35 +1,24 @@
 #include <iostream>
 #define lli long long int
 using namespace std;
-void print_even(lli n){
-      lli start = n;
-      if(n%2!=0){
-            start--;
-      }
-      while(start){cout<<start<<" ";}
-}
-
-void print_odd(lli n){
-      lli end = n;
-      if(n%2!=1){
-            end--;
-      }
-      lli i=1;
-      while(i<=end){cout<<i<<" ";}
-}
 
 int main(int argc, char const *argv[])
 {
       lli n;
       cin>>n;
-      if(n==1)
+      if(n==1){
             std::cout<<1<<std::endl;
+      }
       if(n<4){
             std::cout<<"NO SOLUTION"<<std::endl;
       }
       else{
-            print_even(n);
-            print_odd(n);
+            for(lli i=2; i<=n; i+=2){
+                  cout<<i<<" ";
+            }
+            for(lli i=1;i<=n;i+=2){
+                  cout<<i<<" ";
+            }
             
       }
       return 0;
